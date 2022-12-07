6c5056838cf4e43ada1e0d5e0c559600cd82728f
Dane Cronin <danecronin@gmail.com>
2022-12-07 15:35:33 -0700

    setup ApolloClient and ApolloProvider in client/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,36 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//import Apollo Client 
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/Client';
+
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+//Setup new Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +44,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
