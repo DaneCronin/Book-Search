876ae22021fadb897a2878c5bbf4e7ab33d0afca
Dane Cronin <danecronin@gmail.com>
2022-12-12 18:09:25 -0700

    trying to fix for deploying to Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+//connect to DB using either MONGODB_URI or local host url 
+mongoose.set('debug', true);
+mongoose.set('strictQuery', true);
+
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  cache: "bounded",
   context: authMiddleware
 });
 
