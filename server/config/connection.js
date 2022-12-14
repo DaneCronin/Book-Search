fb21d1a2f2ecc80aa7eb17194d4800d39ee9a24b
Dane Cronin <danecronin@gmail.com>
2022-12-14 12:59:56 -0700

    changed connection.js for mongoose connection:

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,8 +8,8 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
